c0e7f878f7438ac5d4df3c181187f8302c582cc4
sunzheng <2824016820@qq.com>
2023-02-13 22:28:55 +0800

    Initial Commit

---
+++ PoorEngine/PoorEngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoorEngineApp.swift
+//  PoorEngine
+//
+//  Created by 孙政 on 2023/2/13.
+//
+
+import SwiftUI
+
+@main
+struct PoorEngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
