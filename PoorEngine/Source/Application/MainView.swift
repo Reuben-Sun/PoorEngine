ffce22660bac104f418c8148dfbff0853ac9b0c2
Reuben <2824016820@qq.com>
2023-02-14 10:57:23 +0800

    view

--- PoorEngine/Source/Application/MainView.swift
+++ PoorEngine/Source/Application/MainView.swift
@@ -7,12 +7,19 @@
 
 import SwiftUI
 
+let size: CGFloat = 150
+
 struct MainView: View {
     @State var options = Options()
     var body: some View {
         HStack{
-            SceneView(options: options).frame(width: 400, height: 400).border(Color.black, width: 2)
-            InspectorView().frame(width: 200, height: 400).border(Color.black, width: 2)
+            VStack{
+                SceneView(options: options).frame(width:    size * 4, height: size * 3).border(Color.black, width: 2)
+                
+                ContentView().frame(width: size * 4, height: size).border(Color.black, width: 2)
+            }
+            
+            InspectorView().frame(maxWidth: .infinity, maxHeight: .infinity).border(Color.black, width: 2)
         }
         .padding()
     }
