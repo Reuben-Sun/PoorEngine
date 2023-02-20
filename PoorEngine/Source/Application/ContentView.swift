c75239c07b3b29ed87bfc53e0625bef6d4977c19
Reuben <2824016820@qq.com>
2023-02-20 11:48:34 +0800

    content view UI

--- PoorEngine/Source/Application/ContentView.swift
+++ PoorEngine/Source/Application/ContentView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors = ["Red", "Green", "Blue"]
+    var threeColumnGrid = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        //TODO: 点击这些Circle后会切换场景、模型
+        ScrollView{
+            LazyVGrid(columns: threeColumnGrid){
+                ForEach((0...6), id: \.self){item in
+                    Circle().fill(Color.blue)
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
