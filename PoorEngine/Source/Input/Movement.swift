ffa473965fba6f0d5aba1b2756d665f6acbdaf0d
Reuben <2824016820@qq.com>
2023-03-23 19:15:57 +0800

    调整camera移动速度

--- PoorEngine/Source/Input/Movement.swift
+++ PoorEngine/Source/Input/Movement.swift
@@ -10,7 +10,7 @@ import Foundation
 /// 移动参数
 enum Settings {
     static var rotationSpeed: Float {2.0}
-    static var translationSpeed: Float {3.0}
+    static var translationSpeed: Float {5.0}
     static var mouseScrollSensitivity: Float {0.1}
     static var mousePanSensitivity: Float {0.008}
     static var touchZoomSensitivity: Float {10}
